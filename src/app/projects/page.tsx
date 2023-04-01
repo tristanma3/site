d7b7061d56a5e88b6f38510e45cc16714fb5c5f1
tristanma3 <73194028+tristanma3@users.noreply.github.com>
2023-03-31 19:28:48 -0700

    added background for project page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,8 +1,25 @@
+import Image from "next/image";
+
 const Projects = () => {
 	return (
 		<>
-			<div>
-				<h1> Fuck you </h1>
+			<div
+				style={{
+					zIndex: 0,
+					position: "fixed",
+					width: "100vw",
+					height: "100vh",
+					pointerEvents: "none",
+				}}
+			>
+				<Image src="/230804.webp" alt="lolbg" layout="fill" objectFit="cover" />
+			</div>
+			<div
+				style={{
+					zIndex: 1,
+				}}
+			>
+				<h1 className="text-sky-50"> Fuck you </h1>
 			</div>
 		</>
 	);
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,18 +2,23 @@ import Link from "next/link";
 
 export const Navbar = (props: any) => {
 	const navigation = [
-		{ name: "#main", href: "/" },
-		{ name: "#projects", href: "/projects" },
+		{ name: "# main", href: "/" },
+		{ name: "# projects", href: "/projects" },
 	];
 
 	return (
 		<>
-			<div className="w-full flex justify-center items-center gap-6 text-sky-50 min-h-[90px]">
+			<div
+				className="w-full flex justify-center items-center gap-6 text-text-color min-h-[90px]"
+				style={{
+					zIndex: 1,
+				}}
+			>
 				{navigation.map(item => (
 					<Link
 						href={item.href}
 						key={item.name}
-						className="underline underline-offset-2 decoration-2 transition-all ease-in-out duration-200 hover:text-bg-sky-700"
+						className="decoration-2 transition-all ease-in-out duration-200 text-sky-50 hover:text-fuchsia-700"
 					>
 						{item.name}
 					</Link>
